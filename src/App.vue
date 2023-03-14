d499a3a756d0404a6b1ecf0f050f9e3903613a8c
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-14 21:20:44 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+<template>
+
+    <Router-View />
+
+</template>
+
+<script>
+import { MenuIcon, } from "@heroicons/vue/solid";
+
+
+export default {
+  name: "App",
+  components: { MenuIcon,},
+};
+
+</script>
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      "colors": {
+
+      },
+      "fontSize": {
+
+      },
+      "fontFamily": {
+
+      },
+      "boxShadow": {
+
+      },
+      "borderRadius": {
+        
+      }
+    },
+  },
+  plugins: [],
+}
