7b48d96baf3643d0d6ba6b57c54d05d493d39b94
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 18:06:28 +0100

    Router vers la page Mentions legales

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
+import MentionslegalesView from '../views/MentionslegalesView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'AccueilView', component: AccueilView },
+
+    { path: '/Mentionslegales', name: 'MentionslegalesView', component: MentionslegalesView },
   ]
 })
 
