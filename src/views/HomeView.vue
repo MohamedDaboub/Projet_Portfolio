f2117509d06df114e274c11603c6ae2823330420
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 21:34:17 +0100

    import tout les elemet dans la page home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,32 @@
 <template>
-  
+  <div class="px-4">
+    <div id="Accueil">
+        <Accueil  />
+    </div>
+    <div id="APropos" >
+        <Apropos />
+    </div>
+    <div id="Portfolio">
+        <Portfolio />
+    </div>
+    <div id="Experience" >
+        <Experience />
+    </div>
+    <div id="Contact">
+        <Contact />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Accueil from "./Contenu/AccueilView.vue"
+import Apropos from "./Contenu/AProposView.vue"
+import Portfolio from "./Contenu/PortfolioView.vue"
+import Experience from "./Contenu/ExperienceView.vue"
+import Contact from "./Contenu/ContactView.vue"
 
+export default {
+    components: {Accueil,Apropos,Portfolio,Experience,Contact,},
 }
 </script>
 
