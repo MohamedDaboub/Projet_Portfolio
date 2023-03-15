f83f851afa1d4c68c3199a9aa0f48c848b35ca19
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 21:32:06 +0100

    page a propos

---
+++ src/views/Contenu/Projet/AProposView.vue
@@ -0,0 +1,22 @@
+<template>
+    <section id="aPropos" class="max-w-screen-xl mx-auto lg:pt-24 ">
+      <h2 class="font-Iceberg md:text-3xl text-2xl mb-4 text-white">Permettez-moi de me présenter...</h2>
+      <div class=" mx-auto py-2 flex flex-col-reverse lg:flex-row justify-center items-center text-white">
+        <div class="w-full lg:w-1/2 pr-10">
+          <p class="mb-6">
+            Je suis un jeune développeur web, passionné par le design et le
+            développement web. Je suis actuellement en formation de développeur
+            web et web mobile à l'école O'clock.
+          </p>
+        </div>
+        <div class="w-full lg:w-1/2 flex justify-center my-10">
+          <img
+            class="w-3/4 shadow-lg border-2 border-white rounded-full"
+            src="../../assets/image 149.webp"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+</template>
+  
