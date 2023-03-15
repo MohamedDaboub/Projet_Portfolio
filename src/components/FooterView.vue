e0a4a9bd8611b445c834df54f5836f451d4dc37e
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 19:32:43 +0100

    footer.view + css

---
+++ src/components/FooterView.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="bg-Default/me/back text-white border-t-2 py-5">
+        <div class="pt-5 ">
+        <p class="text-center text-lg">  &copy; Daboub.fr {{ new Date().getFullYear() }} - Tous droits réservés |  <RouterLink to="/Mentionslegales">Mentions légales</RouterLink></p>
+        </div>
+    </footer>
+</template>
