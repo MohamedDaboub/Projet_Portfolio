8f8d26ea8476863f0bf888177a4e5a77b479f3ca
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 18:04:35 +0100

    html de la page Mentions legales

--- src/views/MentionslegalesView.vue
+++ src/views/MentionslegalesView.vue
@@ -1,6 +1,33 @@
 <template>
   <section>
-    
+    <div>
+        <h1>Mentions légales</h1>
+    </div>
+    <div>
+        <h2 >Informations sur le propriétaire du site :</h2>
+        <p >Mohamed Daboub</p>
+    </div>
+    <div>
+        <h2 >Hébergeur du site :</h2>
+        <p >Le site est hébergé par la société <span class="border-b-2 "> <a href="https://www.infomaniak.com/">Infomaniak,</a></span> dont le siège social est situé au 7, rue des Alpes, 1227 Carouge, Genève, Suisse,</p>
+        <p >04 11 93 76 67</p>
+    </div>
+    <div>
+        <h2 >Propriété intellectuelle :</h2>
+        <p >Tous les éléments du site (textes, images, vidéos, etc.) sont la propriété exclusive de Mohamed Daboub et sont protégés par les lois relatives à la propriété intellectuelle. Toute reproduction, diffusion, ou exploitation non autorisée de ces éléments est strictement interdite.</p>
+    </div>
+    <div >
+        <h2>Utilisation des données personnelles :</h2>
+        <p >Les informations personnelles recueillies sur ce site sont utilisées uniquement dans le cadre de la gestion de votre compte et de vos échanges avec Mohamed Daboub. Elles ne sont en aucun cas transmises à des tiers. Conformément à la loi Informatique et Libertés du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et de suppression des données vous concernant.</p>
+    </div>
+    <div >
+        <h2>Responsabilité :</h2>
+        <p >Mohamed Daboub décline toute responsabilité en cas d'interruption ou d'indisponibilité du site, de survenance de bugs ou d'erreurs. L'utilisateur est seul responsable de l'utilisation qu'il fait du site et des informations qui y sont contenues.</p>
+    </div>
+    <div >
+        <h2>Liens hypertextes :</h2>
+        <p >Les liens hypertextes présents sur le site peuvent renvoyer vers d'autres sites web. Mohamed Daboub ne saurait être tenu responsable du contenu de ces sites ni des éventuels préjudices qu'ils pourraient causer. Il est important de noter que ces mentions légales ne sont qu'un exemple et qu'il est recommandé de consulter un avocat pour s'assurer de leur pertinence et de leur conformité avec les lois en vigueur dans votre pays.</p>
+    </div>  
   </section>
 </template>
 
