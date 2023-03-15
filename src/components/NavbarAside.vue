a71bda584a303d566bb857a372fec6a1d8d97971
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-15 18:14:07 +0100

    HTML du Navbar Aside

--- src/components/NavbarAside.vue
+++ src/components/NavbarAside.vue
@@ -1,13 +1,42 @@
 <template>
-  
+    <aside >
+        <header >
+          <div >
+            <RouterLink to="/">
+              <img src=""  alt="">
+            </RouterLink>
+          </div>
+        </header>
+        <nav >
+          <ul>
+            <li >
+              <a href=""></a>
+            </li>
+            <li >
+              <a href="" ></a>
+            </li>
+            <li >
+              <a href=""></a>
+            </li>
+            <li >
+              <a href=""></a>
+            </li>
+            <li >
+              <a href=""></a>
+            </li>
+            <li >
+              <a href="" download="" ></a>
+            </li>
+          </ul>
+        </nav>
+    </aside>
 </template>
-
+  
 <script>
 export default {
-
+components: {},
 }
-</script>
 
-<style>
-
+</script> 
+<style>  
 </style>
