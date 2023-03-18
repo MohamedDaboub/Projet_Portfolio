96f17c54e55a0f77331da22d2fdbc62fd275f10a
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-18 21:18:51 +0100

    page 404 fini

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col my-40 items-center justify-center">
-      <h1 class="text-7xl font-bold text-white mt-12">Oops! Page non trouvée</h1>
+    <div class="flex flex-col md:my-40 my-10 items-center text-center justify-center">
+      <h1 class="md:text-7xl text-2xl px-2 font-bold text-white mt-12">Oops! Page non trouvée</h1>
       <p class="text-white mt-4 text-lg">Désolé, la page que vous cherchez n'existe pas.</p>
       <router-link to="/" class="bg-white hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded mt-8">Retourner à la page d'accueil</router-link>
     </div>
