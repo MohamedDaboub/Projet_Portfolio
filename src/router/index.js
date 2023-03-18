c9f819a587f17d7256a17ec59f3d1525a7590668
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-18 20:44:11 +0100

    Création du router vers page 404

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import MentionslegalesView from '../views/MentionslegalesView.vue'
+import page404 from '../views/404View.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'HomeView', component: HomeView },
 
     { path: '/Mentionslegales', name: 'MentionslegalesView', component: MentionslegalesView },
+    { path: '/:pathMatch(.*)*', name: 'page404', component: page404 },
   ],
   scrollBehavior(to, from, savedPosition) {
     // always scroll to top
