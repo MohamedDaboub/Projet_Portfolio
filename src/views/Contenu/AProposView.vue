76e67c5efb02718f84932a707bf7d48a8491ef47
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-18 18:43:41 +0100

    Page Apropos Fini

--- src/views/Contenu/AProposView.vue
+++ src/views/Contenu/AProposView.vue
@@ -4,11 +4,17 @@
       <div class=" mx-auto py-2 flex flex-col-reverse lg:flex-row justify-center items-center text-white">
         <div class="w-full lg:w-1/2 pr-10">
           <p class="mb-6">
-            Je suis un jeune développeur web, passionné par le design et le
-            développement web. Je suis actuellement en formation de développeur
-            web et web mobile à l'école O'clock.
+            Je suis un jeune étudiant de 19 ans nommé Mohamed Daboub. Actuellement inscrit en bachelor universitaire de technologie métiers du multimédia et de l'internet à Montbéliard, j'ai choisi de me spécialiser dans le développement.
           </p>
-        </div>
+          <p class="mb-6">
+            Grâce à mon parcours en MMI, j'ai pu développer des compétences techniques solides, notamment en développement web, en design graphique et en expérience utilisateur. J'ai également pu acquérir des compétences en gestion de projet, en communication et en marketing. 
+          </p>
+          <p>
+            Je suis un passionné du développement depuis plusieurs années et j'ai déjà travaillé sur plusieurs projets, avec pour objectif de répondre aux besoins des clients. Je prends plaisir à créer des sites web et des applications. Je suis également un grand fan de jeux vidéo et de cinéma, ce qui m'a permis de développer des compétences en animation 3D et en modélisation 3D.
+          </p>
+            
+          </div>
+
         <div class="w-full lg:w-1/2 flex justify-center my-10">
           <img
             class="w-3/4 shadow-lg border-2 border-white rounded-full"
