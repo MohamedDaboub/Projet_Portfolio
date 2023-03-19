c43cf2b8f107d356d8b10ddfc73094e197f0fb46
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-03-19 23:14:33 +0100

    correstion v9

--- src/views/Contenu/AccueilView.vue
+++ src/views/Contenu/AccueilView.vue
@@ -1,45 +1,26 @@
-<!-- <template>
-  <section class="h-screen    py-8">
-    <div id="Accueil" class=" m-auto  ">
-      <div class="text-white w-full  pr-10">
-        <h1 class="md:text-4xl text-2xl font-Iceberg mb-4">Mohamed <span>Daboub</span></h1>
-        <div class="m-auto flex  justify-center ">
-          <p>est un étudiant passionné par la création de sites Web et les technologies de développement. Actuellement en deuxième année de MMI (Métiers du Multimédia et de l'Internet), il se spécialise en développement</p>
-          <h4 class="text-2xl font-poppins mb-6"><span class="typing  "></span></h4>
-        </div>
-      </div>
-    </div>
-  </section>
-</template>
-  
-  <script>
-  import Typed from 'typed.js';
-  export default {
-    name: "Accueil",
-    mounted() {
-      const typed = new Typed('.typing', {
-        strings: [''],
-        typeSpeed: 100,
-        backSpeed: 100,
-        loop: true
-      });
-    }
-  }
-  </script>
-   -->
 <template>
   <section id="Accueil">
-    <div class="py-20 px-10">
-      <h1 class="w-1/3 text-4xl ji">
+    <div class="md:py-4 py-8 md:px-10 px-4">
+      <h1 class="md:w-1/3 lg:text-4xl text-2xl py-2">
         Mohamed <span>Daboub</span>
       </h1>
-      <h3 class="md:w-1/2 w-full">Webdesigner & Développeur front-end & Développeur jeux vidéo</h3>
+      <h3 class="md:w-1/2 w-full text-xl md:text-2xl py-1">Webdesigner & Développeur front-end & Développeur jeux vidéo</h3>
+      <p class="md:py-1 py-4">étudiant passionné par la création de sites Web et les technologies de développement. 
+        Actuellement en deuxième année de MMI (Métiers du Multimédia et de l'Internet),</p>
+      <div class="flex justify-center">
+        <a href="#aPropos" class="flex text-center m-auto my-12 fill-white light:fill-black  motion-safe:animate-bounce">
+          <flech class="flex text-center m-auto my-12 fill-white light:fill-black  motion-safe:animate-bounce" />
+        </a>
+      </div>
     </div>
     
   </section>
 </template>  
-<style scoped>
-.ji{
-  line-height: 5rem;
-}
-</style>
+<script>
+import flech from "@/components/icon/Flechbas.vue"
+
+export default {
+    name: "App",
+    components: { flech, },
+};
+</script>
